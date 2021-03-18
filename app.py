5702e095f546193d1efd5fde778456b18e794701
Addison <addisoncox@protonmail.com>
2021-03-18 12:53:10 -0400

    basic database example

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask
-from flask import render_template
-from flask import request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+db = SQLAlchemy(app)
+
+class Analytics(db.Model):
+    
+    id = db.Column('analytics_id', db.Integer, primary_key = True)
+    views = db.Column(db.Integer)
+
+    def __init__(self, views):
+        self.views = views
+    
+    def __repr__(self):
+        return f"{self.views}"
 
 @app.route('/')
 def hello_world():
-    
-    greeting = "Hello World"
-    return greeting
+    if Analytics.query.first() == None:
+        analytics = Analytics(0)
+        db.session.add(analytics)
+        db.session.commit()
+    Analytics.query.first().views = Analytics.query.first().views + 1
+    db.session.commit()
+    return f"This page has been viewed {Analytics.query.first()} times"
 
 if __name__ == "__main__":
-    app.run()
+    db.create_all()
+    app.run(debug=True)
